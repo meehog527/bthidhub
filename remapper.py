c41ee6cdda5266748ff0f99d70fccca68ab9d4c9
meehog527 <52838950+meehog527@users.noreply.github.com>
2021-01-27 00:11:56 -0500

    Update remapper.py

--- remapper.py
+++ remapper.py
@@ -7,18 +7,18 @@ import asyncio_glib
 from adapter import BluetoothAdapter
 from hid_devices import *
 from bluetooth_devices import *
+from gmqtt_cli import MQTTCli
 
 if __name__ == "__main__":
     asyncio.set_event_loop_policy(asyncio_glib.GLibEventLoopPolicy())
     loop = asyncio.get_event_loop()
     bus = SystemMessageBus()
     bluetooth_devices = BluetoothDeviceRegistry(bus, loop)
+    q = MQTTCli(loop, bluetooth_devices)
+    q.connect
     hid_devices = HIDDeviceRegistry(loop)
     hid_devices.set_bluetooth_devices(bluetooth_devices)
     bluetooth_devices.set_hid_devices(hid_devices)
     adapter = BluetoothAdapter(bus, loop, bluetooth_devices, hid_devices)
     web = Web(loop, adapter, bluetooth_devices, hid_devices)
     loop.run_forever()
-
-
-#print(proxy)
